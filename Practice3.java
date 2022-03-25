efbb31701d71f6678c06adb0a9c6355fa2d148e2
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-25 16:17:44 +0900

    0325 Scanner

---
+++ Practice3.java
@@ -0,0 +1,19 @@
+package Mar25th;
+
+import java.util.Scanner;
+
+public class Practice3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int num1,num2;
+		System.out.println("Input the 1st number!");
+		num1 = scanner.nextInt();
+		System.out.println("Input the 1st number!");
+		num2 = scanner.nextInt();
+		
+		System.out.printf("Result : %d + %d = %d",num1,num2,num1+num2);
+	}
+
+}
