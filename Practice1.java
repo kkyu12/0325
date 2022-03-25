e597939dccd86594229066e0b7c8ce19bf208ea4
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-25 16:16:16 +0900

    0325 Hello world!

---
+++ Practice1.java
@@ -0,0 +1,30 @@
+package Mar25th;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!\n\n");
+		
+		System.out.println("Hello world!\nHello world!\nHello world!\n");
+		func1();
+		
+		System.out.println("5x2=10");
+		System.out.println("5x3=15");
+		System.out.println("5x4=20");
+		System.out.println("5x5=25");
+		System.out.println("5x6=30");
+		System.out.println("5x7=35");
+		System.out.println("5x8=40");
+		System.out.println("5x9=45");
+		
+		System.out.println("5x2=10\n5x3=15\n5x4=20\n5x5=25\n5x6=30\n5x7=35\n5x8=40\n5x9=45");
+	}
+	
+	public static void func1() {
+		System.out.println("Hello korea!");
+	}
+
+}
