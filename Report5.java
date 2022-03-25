6633d48e8ed876fcbda75dbef0dd2003ae66f133
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-25 16:55:28 +0900

    0325 if문 예제

---
+++ Report5.java
@@ -0,0 +1,17 @@
+package Mar25th;
+
+public class Report5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		i=3; j=5;
+		if (i > j) {
+			System.out.println("i is greater than j");
+		} else {
+			System.out.println("j is greater than i and i are the same");
+		}
+
+	}
+
+}
