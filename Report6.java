a0c2a50edd8038e472d89edb17eef8c727711b91
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-25 16:55:46 +0900

    0325 switch문 예제

---
+++ Report6.java
@@ -0,0 +1,21 @@
+package Mar25th;
+
+
+public class Report6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 2;
+		switch (n) {
+			case 1:
+				System.out.println("One!");
+				break;
+			case 2:
+				System.out.println("Two!");
+				break;
+			default:
+				System.out.println("Except 1,2");
+				break;
+		}
+	}
+}
