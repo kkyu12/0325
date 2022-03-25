4691324052d18ea4b5a196f4d55b8c158961ec74
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-25 16:19:32 +0900

    0325 Multiplicatione tables

---
+++ Report3.java
@@ -0,0 +1,72 @@
+package Mar25th;
+
+public class Report3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("2 x 2 = 4\n");
+		System.out.print("2 x 3 = 6\n");
+		System.out.print("2 x 4 = 8\n");
+		System.out.print("2 x 5 = 10\n");
+		System.out.print("2 x 6 = 12\n");
+		System.out.print("2 x 7 = 14\n");
+		System.out.print("2 x 8 = 16\n");
+		System.out.print("2 x 9 = 18\n");
+		System.out.print("3 x 2 = 6\n");
+		System.out.print("3 x 3 = 9\n");
+		System.out.print("3 x 4 = 12\n");
+		System.out.print("3 x 5 = 15\n");
+		System.out.print("3 x 6 = 18\n");
+		System.out.print("3 x 7 = 21\n");
+		System.out.print("3 x 8 = 24\n");
+		System.out.print("3 x 9 = 27\n");
+		System.out.print("4 x 2 = 8\n");
+		System.out.print("4 x 3 = 12\n");
+		System.out.print("4 x 4 = 16\n");
+		System.out.print("4 x 5 = 20\n");
+		System.out.print("4 x 6 = 24\n");
+		System.out.print("4 x 7 = 28\n");
+		System.out.print("4 x 8 = 32\n");
+		System.out.print("4 x 9 = 36\n");
+		System.out.print("5 x 2 = 10\n");
+		System.out.print("5 x 3 = 15\n");
+		System.out.print("5 x 4 = 20\n");
+		System.out.print("5 x 5 = 25\n");
+		System.out.print("5 x 6 = 30\n");
+		System.out.print("5 x 7 = 35\n");
+		System.out.print("5 x 8 = 40\n");
+		System.out.print("5 x 9 = 45\n");
+		System.out.print("6 x 2 = 12\n");
+		System.out.print("6 x 3 = 18\n");
+		System.out.print("6 x 4 = 24\n");
+		System.out.print("6 x 5 = 30\n");
+		System.out.print("6 x 6 = 36\n");
+		System.out.print("6 x 7 = 42\n");
+		System.out.print("6 x 8 = 48\n");
+		System.out.print("6 x 9 = 54\n");
+		System.out.print("7 x 2 = 14\n");
+		System.out.print("7 x 3 = 21\n");
+		System.out.print("7 x 4 = 28\n");
+		System.out.print("7 x 5 = 35\n");
+		System.out.print("7 x 6 = 42\n");
+		System.out.print("7 x 7 = 49\n");
+		System.out.print("7 x 8 = 56\n");
+		System.out.print("7 x 9 = 63\n");
+		System.out.print("8 x 2 = 16\n");
+		System.out.print("8 x 3 = 24\n");
+		System.out.print("8 x 4 = 32\n");
+		System.out.print("8 x 5 = 40\n");
+		System.out.print("8 x 6 = 48\n");
+		System.out.print("8 x 7 = 56\n");
+		System.out.print("8 x 8 = 64\n");
+		System.out.print("8 x 9 = 72\n");
+		System.out.print("9 x 2 = 18\n");
+		System.out.print("9 x 3 = 27\n");
+		System.out.print("9 x 4 = 36\n");
+		System.out.print("9 x 5 = 45\n");
+		System.out.print("9 x 6 = 54\n");
+		System.out.print("9 x 7 = 63\n");
+		System.out.print("9 x 8 = 72\n");
+		System.out.print("9 x 9 = 81\n");
+	}
+}
